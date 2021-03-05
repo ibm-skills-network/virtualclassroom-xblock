bbbdf74655434811e41fc113cd497d4b393b7b86
Michael Roudnitski <michaelroudnitski@gmail.com>
2021-03-04 19:32:17 -0500

    Change version

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ def package_data(pkg, roots):
 
 setup(
     name='lti_consumer-xblock',
-    version='1.2.6',
+    version='1.2.6-extended',
     description='This XBlock implements the consumer side of the LTI specification.',
     packages=[
         'lti_consumer',
